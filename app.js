8df68a97fbbbdec0c298ce7662d31ddbe90f7d77
Jeremy <jeremy.ohearn1@gmail.com>
2023-11-22 17:31:38 -0500

    added geolocation logic to get current position

--- app.js
+++ app.js
@@ -3,7 +3,33 @@
 const appImg = document.querySelector(".app-image");
 const locationTitle = document.querySelector(".location");
 const locationInput = document.getElementById("location-input");
-const sunriseText = document.quesrySelector(".sunrise");
-const sunsetText = document.quesrySelector(".sunset");
-const tempText = document.quesrySelector(".temp");
-const windText = document.quesrySelector(".wind");
+const sunriseText = document.querySelector(".sunrise");
+const sunsetText = document.querySelector(".sunset");
+const tempText = document.querySelector(".temp");
+const windText = document.querySelector(".wind");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Cannot access location data");
+  }
+}
+
+getLocation();
+function showPosition(position) {
+  const coords = position.coords;
+  console.log(coords);
+}
+
+const request = new XMLHttpRequest();
+
+request.open(
+  "GET",
+  "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+);
+
+request.addEventListener("load", function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+});
