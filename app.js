e10d9a112f99ec0b37376a9b8afe0e291d9a3550
Jeremy <jeremy.ohearn1@gmail.com>
2023-11-22 17:01:20 -0500

    declare variables in javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+// Declare variables
+
+const appImg = document.querySelector(".app-image");
+const locationTitle = document.querySelector(".location");
+const locationInput = document.getElementById("location-input");
+const sunriseText = document.quesrySelector(".sunrise");
+const sunsetText = document.quesrySelector(".sunset");
+const tempText = document.quesrySelector(".temp");
+const windText = document.quesrySelector(".wind");
