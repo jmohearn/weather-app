b9010b960b1ee87e8d0fa658e30a93bf5f53828f
Jeremy <jeremy.ohearn1@gmail.com>
2023-11-22 20:14:27 -0500

    added font awesome, began implementing logic for weather data from api

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const sunriseText = document.querySelector(".sunrise");
 const sunsetText = document.querySelector(".sunset");
 const tempText = document.querySelector(".temp");
 const windText = document.querySelector(".wind");
+const searchBtn = document.querySelector(".search-btn");
+const apiKey = "a9083f84ed84924bb939a800022aae5c";
+let data;
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -17,19 +20,37 @@ function getLocation() {
 }
 
 getLocation();
+
 function showPosition(position) {
-  const coords = position.coords;
-  console.log(coords);
+  const { latitude, longitude } = position.coords;
+  console.log(latitude, longitude);
+
+  const request = new XMLHttpRequest();
+
+  request.open(
+    "GET",
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+  );
+
+  request.send();
+
+  request.addEventListener("load", function () {
+    data = JSON.parse(this.responseText);
+    console.log(data);
+    updateUI(data);
+  });
 }
 
-const request = new XMLHttpRequest();
+function updateUI(data) {
+  const temp = data.main.temp;
+  const highTemp = data.main.temp_max;
+  const lowTemp = data.main.temp_min;
+  const locationName = data.main.name;
 
-request.open(
-  "GET",
-  "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
-);
+  function kelvinToF(f) {
+    const tempF = Math.round((Number(f) - 273.15) * (9 / 5) + 32);
+    return tempF;
+  }
 
-request.addEventListener("load", function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-});
+  console.log(kelvinToF(temp), kelvinToF(highTemp), kelvinToF(lowTemp));
+}
