5741b7797d9daa71d37d4889a5f093a322d971af
Jeremy <jeremy.ohearn1@gmail.com>
2023-11-24 12:17:11 -0500

    wrote logic for updating ui for current location, conditions, high, low, wind speed and direction

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // Declare variables
 
 const appImg = document.querySelector(".app-image");
-const locationTitle = document.querySelector(".location");
+const locationTitle = document.querySelector("#location");
 const locationInput = document.getElementById("location-input");
 const sunriseText = document.querySelector(".sunrise");
 const sunsetText = document.querySelector(".sunset");
-const tempText = document.querySelector(".temp");
-const windText = document.querySelector(".wind");
+const currentTempText = document.querySelector("#current-temp");
+const highTempText = document.querySelector("#temp-high");
+const lowTempText = document.querySelector("#temp-low");
+const windText = document.querySelector("#wind");
 const searchBtn = document.querySelector(".search-btn");
 const apiKey = secret.key;
 let data;
@@ -48,19 +50,72 @@ function updateUI(data) {
   const locationName = data.name;
   const sunrise = data.sys.sunrise;
   const sunset = data.sys.sunset;
+  const clouds = data.weather[0].description;
+  const windSpeed = data.wind.speed;
+  let windDirectionDeg = data.wind.deg;
+  let windDirectionQuad;
+
+  function getQuad(degRaw) {
+    let degree = Number(degRaw);
+    let quad;
+    if (degree === 0) {
+      quad = "N";
+    } else if (degree > 0 && degree < 90) {
+      quad = "NE";
+    } else if (degree === 90) {
+      quad = "NE";
+    } else if (degree > 90 && degree < 180) {
+      quad = "SE";
+    } else if (degree === 180) {
+      quad = "S";
+    } else if (degree > 180 && degree < 270) {
+      quad = "SW";
+    } else if (degree === 270) {
+      quad = "W";
+    } else if (degree > 270 && degree < 359) {
+      quad = "NW";
+    } else {
+      quad = "No wind data";
+    }
+    return quad;
+  }
 
   function kelvinToF(f) {
     const tempF = Math.round((Number(f) - 273.15) * (9 / 5) + 32);
     return tempF;
   }
 
+  locationTitle.textContent = locationName;
+  currentTempText.innerHTML = `
+  <h2>Now: ${kelvinToF(temp)}&deg; F &amp; ${clouds}</h2>
+  `;
+  highTempText.innerHTML = `
+  <h2 class="app-info--text temp">
+            <i class="fa-solid fa-temperature-arrow-up"></i> ${kelvinToF(
+              highTemp
+            )}&deg; F
+          </h2>
+  `;
+  lowTempText.innerHTML = `
+  <h2 class="app-info--text temp">
+            <i class="fa-solid fa-temperature-arrow-up"></i> ${kelvinToF(
+              lowTemp
+            )}&deg; F
+          </h2>
+  `;
+  windText.innerHTML = `
+  <i class="fa-solid fa-wind"></i>
+          <h2 class="app-info--text wind">${Math.round(
+            windSpeed * 2.237
+          )} mph from ${getQuad(windDirectionDeg)}</h2>
+  `;
+
   console.log(
-    `Current temperature in ${locationName} is: ${kelvinToF(
+    `Current temperature in ${locationName} is:${kelvinToF(
       temp
-    )} degrees F, Today's high is: ${kelvinToF(
-      highTemp
-    )} degrees F, Today's Low is: ${kelvinToF(
-      lowTemp
-    )} degrees F. Sunrise: ${sunrise} Sunset: ${sunset}`
+    )} degrees F, Today's high is: ${kelvinToF(highTemp)}
+     degrees F, Today's Low is: ${kelvinToF(
+       lowTemp
+     )} degrees F. Sunrise: ${sunrise} Sunset: ${sunset}${clouds}`
   );
 }
